11ff18a26134e8bda2ad146c2fce07d4aa5727db
David Smith <djsmith611@me.com>
2024-03-20 20:22:27 -0700

    called GET on page load with useEffect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
@@ -30,6 +30,10 @@ function App() {
         alert("Something went wrong.");
       });
   };
+
+  useEffect(() => {
+    loadTasks();
+  }, []);
 
   return (
     <div className="app">
