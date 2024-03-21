207e3eab5ad9e0a7e89e4bfb2c2cb2a93cb5cf7d
David Smith <djsmith611@me.com>
2024-03-20 21:11:28 -0700

    created POST in router

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -1,13 +1,29 @@
-const express = require('express');
-const router = express.Router();
-const pool = require('../modules/pool.js');
+const express = require("express");
+const todoRouter = express.Router();
+const pool = require("../modules/pool.js");
 
 // GET
 
 // POST
+todoRouter.post("/", (req, res) => {
+  const task = req.body;
+  const sqlText = `
+  INSERT INTO tasks (todo, complete, toggleText) 
+  VALUES ($1, $2, $3);
+  `;
+  pool
+    .query(sqlText, [task.todo, task.complete, task.toggleText])
+    .then((result) => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.error(`ERROR in ROUTER POST${sqlText}:`, error);
+      res.sendStatus(500);
+    });
+});
 
 // PUT
 
 // DELETE
 
-module.exports = router;
+module.exports = todoRouter;
