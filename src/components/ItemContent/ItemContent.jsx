29063255ae4acf27317cda7f09ad5cda57fa4b13
David Smith <djsmith611@me.com>
2024-04-03 17:47:27 -0700

    changed checkbox inputs to MUI Checkbox

--- src/components/ItemContent/ItemContent.jsx
+++ src/components/ItemContent/ItemContent.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import "./ItemContent.css";
 import ToggleComplete from "../Requests/ToggleComplete.jsx";
+import Checkbox from '@mui/material/Checkbox';
 
 function ItemContent({ task, setTaskList}) {
+  const label = { inputProps: { 'aria-label': task.todo } };
   return (
     <div className="ItemContent">
-      <input
+      <Checkbox
         className="ItemContent-input"
-        type="checkbox"
+        {...label}
         checked={task.complete}
-        onChange={() => ToggleComplete(task.id, setTaskList)}
-      />
+        onChange={() => ToggleComplete(task.id, setTaskList)} 
+        />
       <span className="ItemContent-task"> {task.todo} </span>
     </div>
   );
