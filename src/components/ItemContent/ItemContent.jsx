ff372c4ff18683b8946385678acd286c89f209ff
David Smith <djsmith611@me.com>
2024-04-03 18:39:06 -0700

    added MUI components to item content component

--- src/components/ItemContent/ItemContent.jsx
+++ src/components/ItemContent/ItemContent.jsx
@@ -2,19 +2,41 @@ import React from "react";
 import "./ItemContent.css";
 import ToggleComplete from "../Requests/ToggleComplete.jsx";
 import Checkbox from '@mui/material/Checkbox';
+import Divider from '@mui/material/Divider';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 function ItemContent({ task, setTaskList}) {
   const label = { inputProps: { 'aria-label': task.todo } };
   return (
-    <div className="ItemContent">
+    <Box sx={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
       <Checkbox
         className="ItemContent-input"
         {...label}
         checked={task.complete}
-        onChange={() => ToggleComplete(task.id, setTaskList)} 
-        />
-      <span className="ItemContent-task"> {task.todo} </span>
-    </div>
+        onChange={() => ToggleComplete(task.id, setTaskList)}
+        sx={{ 
+          marginRight: '10px',
+          marginLeft: '10px',
+          marginTop: '10px',
+          marginBottom: '10px', 
+          '& .MuiSvgIcon-root': {
+            width: 30,
+            height: 30
+          }
+        }}
+      />
+      <Divider orientation="vertical" variant="middle" flexItem />
+      <Typography 
+        variant="h5" 
+        className="ItemContent-task" 
+        sx={{ 
+          marginLeft: '10px'}} gutterBottom> {task.todo} </Typography>
+    </Box>
   );
 }
 export default ItemContent;
