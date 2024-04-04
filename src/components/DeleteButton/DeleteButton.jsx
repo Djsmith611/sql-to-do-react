d43a5b62e8d0f3ecabec5a4ba235ad2f496f28d4
David Smith <djsmith611@me.com>
2024-04-03 18:55:38 -0700

    changed remove button to trashcan icon

--- src/components/DeleteButton/DeleteButton.jsx
+++ src/components/DeleteButton/DeleteButton.jsx
@@ -1,13 +1,21 @@
-import React from "react";
 import "./DeleteButton.css";
 import DeleteTask from "../Requests/DeleteTask.jsx";
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
 
 
 function DeleteButton({ task, setTaskList }) {
   return (
-    <button className="Delete" onClick={() => DeleteTask(task.id, setTaskList)}>
-      Remove
-    </button>
+    <IconButton 
+      aria-label="delete" 
+      onClick={() => DeleteTask(task.id, setTaskList)}
+      sx={{
+        '&:hover': {
+          color: 'red'
+        }
+      }}>
+      <DeleteIcon />
+    </IconButton>
   );
 }
 
