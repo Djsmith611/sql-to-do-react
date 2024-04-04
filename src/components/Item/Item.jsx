c22c1d1a8bf9f243fd5ecf50cff323fee83c0e5b
David Smith <djsmith611@me.com>
2024-04-03 18:49:02 -0700

    changed div to Box

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import "./Item.css";
 import ItemContent from "../ItemContent/ItemContent.jsx";
 import DeleteButton from "../DeleteButton/DeleteButton.jsx";
+import Box from '@mui/material/Box';
 
 function Item({task, isEditMode, setTaskList}) {
     const itemClasses = `Item ${task.complete ? 'complete' : 'incomplete'}`
 
     return(
-        <div className={itemClasses}>
+        <Box className={itemClasses}>
           <ItemContent task={task} setTaskList={setTaskList} />
           {isEditMode && (
             <DeleteButton task={task} setTaskList={setTaskList} />
           )}
-        </div>
+        </Box>
     );
 }
 
